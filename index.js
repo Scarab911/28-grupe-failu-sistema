0db9d1debcd9bbc0539b6d852b634448dbfbb3c5
Paulius B <Paulspremium@gmail.com>
2021-07-24 11:35:07 +0300

    maryte object after read

--- index.js
+++ index.js
@@ -13,11 +13,31 @@ _data.read('users', 'petras', (err, data) => {
     console.log(obj.car);
 })
 
-// _data.read('users', 'maryte', (err, data) => {
+
+_data.read('users', 'maryte', (err, data) => {
+    if (err || !data) {
+        console.log('Nepavyko perskaityti failo...');
+        return false;
+    }
+    const obj = JSON.parse(data);
+    console.log(obj);
+    console.log(obj.name);
+    console.log(obj.age);
+    console.log(obj.car);
+
+})
+
+
+//Specialiai su failintas iskvietimas!
+// _data.read('users', 'kebabas', (err, data) => {
 //     if (err || !data) {
-//         console.log('Nepavyko perskaityti failo...');
+//         console.log(err);
 //         return false;
 //     }
 
-//     console.log(data);
+//     const obj = JSON.parse(data);
+//     console.log(obj);
+//     console.log(obj.name);
+//     console.log(obj.age);
+//     console.log(obj.car);
 // })
