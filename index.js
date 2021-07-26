00ce7a84cc483687cf58adb9bc2c7e9ba10e0a83
Paulius B <paulspremium@gmail.com>
2021-07-26 19:20:51 +0300

    file system create

--- index.js
+++ index.js
@@ -8,36 +8,45 @@ _data.read('users', 'petras', (err, data) => {
 
     const obj = JSON.parse(data);
     console.log(obj);
-    console.log(obj.name);
-    console.log(obj.age);
-    console.log(obj.car);
 })
 
 
-_data.read('users', 'maryte', (err, data) => {
+const pazymiai = [10, 2, 8, 4, 6];
+const pazymiai2 = ['penki', 'septyni', 'astuoni', 'trys', 'keturi'];
+
+_data.create('marks', 'maryte', pazymiai2, (err) => {
+    if (err) {
+        console.log(err);
+        return false;
+    }
+
+    console.log('Failas sekmingai sukurtas!');
+
+    _data.read('marks', 'maryte', (err, data) => {
+        if (err || !data) {
+            console.log('Nepavyko perskaityti Kazio failo...');
+            return false;
+        }
+
+        const obj = JSON.parse(data);
+        console.log(obj);
+    })
+});
+_data.read('marks', 'kazys', (err, data) => {
     if (err || !data) {
         console.log('Nepavyko perskaityti failo...');
         return false;
     }
+
     const obj = JSON.parse(data);
     console.log(obj);
-    console.log(obj.name);
-    console.log(obj.age);
-    console.log(obj.car);
-
 })
+_data.read('marks', 'maryte', (err, data) => {
+    if (err || !data) {
+        console.log('Nepavyko perskaityti failo...');
+        return false;
+    }
 
-
-//Specialiai su failintas iskvietimas!
-// _data.read('users', 'kebabas', (err, data) => {
-//     if (err || !data) {
-//         console.log(err);
-//         return false;
-//     }
-
-//     const obj = JSON.parse(data);
-//     console.log(obj);
-//     console.log(obj.name);
-//     console.log(obj.age);
-//     console.log(obj.car);
-// })
+    const obj = JSON.parse(data);
+    console.log(obj);
+})
