ad87bd6b5bb86459803382666a98172e3dbdbfd4
Paulius B <paulspremium@gmail.com>
2021-07-29 18:32:59 +0300

    try n Catch method

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const _data = require('./lib/data');
+const helpers = require('./lib/helpers');
 
-// _data.read('users', 'petras', (err, data) => {
-//     if (err || !data) {
-//         console.log('Nepavyko perskaityti failo...');
-//         return false;
-//     }
+_data.read('users', 'petras', (err, data) => {
+    if (err || !data) {
+        console.log('Nepavyko perskaityti failo...');
+        return false;
+    }
 
-//     const obj = JSON.parse(data);
-//     console.log(obj);
-// })
+    console.log(data);
+    const obj = helpers.parseJsonToObject(data);
+    console.log(obj);
+})
 
 
 // const pazymiai = [10, 2, 8, 4, 6];
@@ -26,32 +28,41 @@ const _data = require('./lib/data');
 //             return false;
 //         }
 
-//         const obj = JSON.parse(data);
+//         const obj = helpers.parseJsonToObject(data);
 //         console.log(obj);
 //     })
 // });
 
-const petras = {
-    name: 'Petras',
-    age: 99,
-    car: 'Audi',
-    color: 'red'
-}
-_data.update('users', 'petras', petras, (err) => {
-    if (err) {
-        console.log(err);
-        return false;
-    }
+// const petras = {
+//     name: 'Petras',
+//     age: 99,
+//     car: 'Audi',
+//     color: 'red'
+// }
+// _data.update('users', 'petras', petras, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return false;
+//     }
 
-    console.log('Petro duomenys atnaujinti sekmingai.');
+//     console.log('Petro duomenys atnaujinti sekmingai.');
 
-    _data.read('users', 'petras', (err, data) => {
-        if (err || !data) {
-            console.log('Nepavyko perskaityti Petro failo...');
-            return false;
-        }
+//     _data.read('users', 'petras', (err, data) => {
+//         if (err || !data) {
+//             console.log('Nepavyko perskaityti Petro failo...');
+//             return false;
+//         }
 
-        const obj = JSON.parse(data);
-        console.log(obj);
-    })
-});
+//         const obj = helpers.parseJsonToObject(data);
+//         console.log(obj);
+//     })
+// });
+
+// _data.delete('users', 'maryte', (err) => {
+//     if (err) {
+//         console.log(err);
+//         return false;
+//     }
+
+//     console.log('Failas sekmingai istrintas.');
+// })
